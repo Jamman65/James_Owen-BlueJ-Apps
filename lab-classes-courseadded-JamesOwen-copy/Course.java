37be9e2811c0123fd4a659c0d7b62abf16fc6033
Jamman65 <71873396+Jamman65@users.noreply.github.com>
2020-10-16 09:19:36 +0100

    Add files via upload
    lab class files

---
+++ lab-classes-courseadded-JamesOwen-copy/Course.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class Course here.
+ *
+ * @author James Owen
+ * @version 0.1
+ */
+public class Course
+{
+    // instance variables - replace the example below with your own
+    // the course that the student can choose
+    public String course;
+    // the id of the course that the student has chosen
+    public String courseid;
+
+    /**
+     * Set a new course for this student.
+     */
+   public class changeCourse {
+       public String newcourse() {
+    System.out.println("Computing G400");
+    return course;
+    
+  }
+}
+
+      /**
+     * Set a new course for this student.
+     */
+    public void changecourse(String replacementcourse, String id1)
+    {
+        course = replacementcourse;
+        courseid = id1;
+    }
+    
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    
+    
+}
